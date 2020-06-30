df7d49fdc7b3ae85efe76b233b9c7faf87759291
Fabio Frank <fabio.frank@hotmail.de>
2020-06-30 14:13:27 +0200

    Funktionen für Fahrwiderstände

--- Antriebsstrang.py
+++ Antriebsstrang.py
@@ -1,20 +1,43 @@
 import numpy as np
 from scipy import constants
 
-#physikalische Konstanten
+# physikalische Konstanten
 g = constants.g
+luftdichte = 1.225 # in kg/m³
 
-#feste Bus-Parameter
-masse = 12000 #in kg
-f_roll = 0.015 #Rollwiderstandbeiwert [ohne Einheit]
+# feste Parameter
+masse = 12000  # in kg
+f_roll = 0.015  # Rollwiderstandbeiwert [ohne Einheit]
+stirnflaeche = 8.8
+c_w = 0.3  # Luftwiderstandsbeiwert
 
-#Input: Ist-Geschwindigkeit in m/s, gewählte Beschleunigung in m/s² aus Fahrermodell
+# Input: Ist-Geschwindigkeit in m/s, gewählte Beschleunigung in m/s² aus Fahrermodell
 v_ist = 10.0
-a_gewählt = 1.0
-alpha = 5
+a_gewaehlt = 1.0
+steigung_prozent = 5
+alpha = np.arctan(steigung_prozent / 100)
+
 
 def rollwiderstand():
-    return rollwiderstand = masse * g * np.cos(alpha) * f_roll
+    return masse * g * np.cos(alpha) * f_roll
 
-fahrwiderstände = rollwiderstand() + beschleunigungswiderstand() + luftwiderstand(v_ist) + steigungswiderstand()
-benötigte_leistung = fahrwiderstände * v_ist
+
+def beschleunigungswiderstand():
+    return masse * a_gewaehlt
+
+
+def luftwiderstand():
+    return 0.5 * stirnflaeche * c_w * luftdichte * (v_ist ** 2)
+
+
+def steigungswiderstand():
+    return masse * g * np.sin(alpha)
+
+
+print("Rollwiderstand: ", rollwiderstand())
+print("Beschleunigungswiderstand: ", beschleunigungswiderstand())
+print("Luftwiderstand: ", luftwiderstand())
+print("Steigungswiderstand: ", steigungswiderstand())
+
+fahrwiderstaende = rollwiderstand() + beschleunigungswiderstand() + luftwiderstand() + steigungswiderstand()
+benoetigte_leistung = fahrwiderstaende * v_ist

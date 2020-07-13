fda68a0ee0fab48a3eb3f995016d01a09900006f
Fabio Frank <fabio.frank@hotmail.de>
2020-07-13 18:11:18 +0200

    Output-Tabelle erstellt

--- Main_Energieverbrauch_je_Umlauf.py
+++ Main_Energieverbrauch_je_Umlauf.py
@@ -1,4 +1,8 @@
-# TODO: Umgang mit NaN-Werten, die auftreten, wenn in der Input-Routendatei keine Steigung angegeben ist für bestimmte Stelle
+# TODO: Umgang mit NaN-Werten, die auftreten, wenn in der Input-Routendatei keine Steigung angegeben ist für
+#  bestimmte Stelle
+
+import numpy as np
+import pandas as pd
 
 from Fahrzeugkomponenten import Fahrzeug, Nebenverbraucher, Batterie, Leistungselektronik, Elektromotor, Getriebe
 import Fahrer
@@ -30,10 +34,18 @@ v_ist = 0.0  # Ist-Geschwindigkeit in m/s
 zurueckgelegte_distanz = 0.0  # zurückgelegte Strecke in m
 kumulierter_energieverbrauch_joule = 0.0
 
+# Erstellen der Datentabelle
+spalten = np.array(
+    ['Zeit [s]', 'SoC [%]', 'Zurückgelegte Distanz [m]', 'Ist-Geschwindigkeit zum Zeitpunkt t [m/s]',
+     'Soll-Geschwindigkeit zum Zeitpunkt t [m/s]', 'Steigung im Intervall [t, t+1) [%]',
+     'Gewählte Beschleunigung im Intervall [t, t+1) [m/s²]', 'Abgerufene Batterieleistung im Intervall [t, t+1) [W]',
+     'Kumulierter Energieverbrauch nach Intervall [t, t+1) [kWh]'])
+umlauf_array = pd.DataFrame(columns=spalten)
+
 # Schleife, die läuft bis Umlauf beendet
 while zurueckgelegte_distanz < streckenlaenge:
     # TODO: Überlegen, was gehört zu t=0, was gehört zu t=1? Größen am Anfang/am Ende des betrachteten Intervalls
-    # TODO: Output-Array erstellen anstatt print-Befehle, evtl. pandas Dataframe
+
     print("Intervall t = [", t, ",", t + zeit_intervall, ")")
 
     # in Abhängigkeit der bereits zurückgelegten Distanz werden aktuelle Steigung sowie Soll-Geschwindigkeit aus der
@@ -64,6 +76,15 @@ while zurueckgelegte_distanz < streckenlaenge:
     print("Kumulierter Energieverbrauch: ", kumulierter_energieverbrauch_joule, "Joule / ",
           kumulierter_energieverbrauch_kWh, " kWh")
 
+    # Füge neu gewonnene Daten zum Array hinzu
+    neue_zeile = {'Zeit [s]': t, 'SoC [%]': neuer_soc,
+                  'Zurückgelegte Distanz [m]': zurueckgelegte_distanz, 'Ist-Geschwindigkeit zum Zeitpunkt t [m/s]': v_ist,
+                  'Soll-Geschwindigkeit zum Zeitpunkt t [m/s]': v_soll, 'Steigung im Intervall [t, t+1) [%]': steigung,
+                  'Gewählte Beschleunigung im Intervall [t, t+1) [m/s²]': beschleunigung,
+                  'Abgerufene Batterieleistung im Intervall [t, t+1) [W]': leistung_batterie,
+                  'Kumulierter Energieverbrauch nach Intervall [t, t+1) [kWh]': kumulierter_energieverbrauch_kWh}
+    umlauf_array = umlauf_array.append(neue_zeile, ignore_index=True)
+
     # Berechnung der zurückgelegten Strecke und der neuen Ist-Geschwindigkeit
     zurueckgelegte_distanz += 0.5 * beschleunigung * (zeit_intervall ** 2) + v_ist * zeit_intervall
     print("Zurückgelegte Strecke: ", zurueckgelegte_distanz, "m")
@@ -72,3 +93,6 @@ while zurueckgelegte_distanz < streckenlaenge:
     t += zeit_intervall
 
     print("_________________________________________________________________________________________")
+
+print(umlauf_array)
+umlauf_array.to_excel('Output.xlsx')

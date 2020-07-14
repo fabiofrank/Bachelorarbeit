f8ad477e1c6dfc706235843db32532c4e173bca3
Fabio Frank <fabio.frank@hotmail.de>
2020-07-14 10:37:03 +0200

    liste.append() ersetzt DataFrame.append()

--- Main_Energieverbrauch_je_Umlauf.py
+++ Main_Energieverbrauch_je_Umlauf.py
@@ -3,10 +3,15 @@
 
 import numpy as np
 import pandas as pd
-
 from Fahrzeugkomponenten import Fahrzeug, Nebenverbraucher, Batterie, Leistungselektronik, Elektromotor, Getriebe
 import Fahrer
 import Route
+from datetime import datetime
+
+start = datetime.now()
+
+# Daten des Umlaufs
+nummer = '1'
 
 # Die Route wird mittels CSV-Datei eingelesen
 route = Route.einlesen('Testdatensatz_10 Zeilen.csv')
@@ -33,66 +38,49 @@ t = 0  # Zeit in s
 v_ist = 0.0  # Ist-Geschwindigkeit in m/s
 zurueckgelegte_distanz = 0.0  # zurückgelegte Strecke in m
 kumulierter_energieverbrauch_joule = 0.0
-
-# Erstellen der Datentabelle
-spalten = np.array(
-    ['Zeit [s]', 'SoC [%]', 'Zurückgelegte Distanz [m]', 'Ist-Geschwindigkeit zum Zeitpunkt t [m/s]',
-     'Soll-Geschwindigkeit zum Zeitpunkt t [m/s]', 'Steigung im Intervall [t, t+1) [%]',
-     'Gewählte Beschleunigung im Intervall [t, t+1) [m/s²]', 'Abgerufene Batterieleistung im Intervall [t, t+1) [W]',
-     'Kumulierter Energieverbrauch nach Intervall [t, t+1) [kWh]'])
-umlauf_array = pd.DataFrame(columns=spalten)
+liste = []
 
 # Schleife, die läuft bis Umlauf beendet
 while zurueckgelegte_distanz < streckenlaenge:
     # TODO: Überlegen, was gehört zu t=0, was gehört zu t=1? Größen am Anfang/am Ende des betrachteten Intervalls
 
-    print("Intervall t = [", t, ",", t + zeit_intervall, ")")
-
     # in Abhängigkeit der bereits zurückgelegten Distanz werden aktuelle Steigung sowie Soll-Geschwindigkeit aus der
     # Routendatei ermittelt
     steigung = Route.steigung(zurueckgelegte_distanz, route)
-    print("Steigung: ", steigung, " %")
     v_soll = Route.v_soll(zurueckgelegte_distanz, route)
-    print("Soll-Geschwindigkeit: ", v_soll, " m/s")
 
     # Der Fahrer wählt in Abhängigkeit von Soll- und Ist-Geschwindigkeit eine Beschleunigung oder Verzögerung aus
     beschleunigung = Fahrer.beschleunigung(v_ist, v_soll)
-    print("Gewählte Beschleunigung: ", beschleunigung, " m/s²")
 
     # Ermittlung des Gesamtleistungsbedarfs
     fahrwiderstaende = Fahrzeug.fahrwiderstaende(v_ist, beschleunigung, steigung)
     benoetigte_leistung = Elektromotor.leistung(fahrwiderstaende, v_ist) + Nebenverbraucher.leistung
     leistung_batterie = Batterie.leistung(benoetigte_leistung)
-    print("Momentane Leistung an der Batterie: ", leistung_batterie, " Watt")
 
     # Berechnung des Energieverbrauchs während des gewählten Zeitintervalls, Entladen bzw. Aufladen der Batterie
     energieverbrauch_im_intervall = leistung_batterie * zeit_intervall
     neuer_soc = Batterie.state_of_charge(energieverbrauch_im_intervall)
-    print("Neuer SoC: ", neuer_soc, " %")
 
     # Aktualisieren des Gesamtenergieverbrauchs im Umlauf
     kumulierter_energieverbrauch_joule += energieverbrauch_im_intervall
     kumulierter_energieverbrauch_kWh = kumulierter_energieverbrauch_joule / 3600000
-    print("Kumulierter Energieverbrauch: ", kumulierter_energieverbrauch_joule, "Joule / ",
-          kumulierter_energieverbrauch_kWh, " kWh")
 
-    # Füge neu gewonnene Daten zum Array hinzu
+    # Sammle neu gewonnene Daten in Liste
     neue_zeile = {'Zeit [s]': t, 'SoC [%]': neuer_soc,
-                  'Zurückgelegte Distanz [m]': zurueckgelegte_distanz, 'Ist-Geschwindigkeit zum Zeitpunkt t [m/s]': v_ist,
+                  'Zurückgelegte Distanz [m]': zurueckgelegte_distanz,
+                  'Ist-Geschwindigkeit zum Zeitpunkt t [m/s]': v_ist,
                   'Soll-Geschwindigkeit zum Zeitpunkt t [m/s]': v_soll, 'Steigung im Intervall [t, t+1) [%]': steigung,
                   'Gewählte Beschleunigung im Intervall [t, t+1) [m/s²]': beschleunigung,
                   'Abgerufene Batterieleistung im Intervall [t, t+1) [W]': leistung_batterie,
                   'Kumulierter Energieverbrauch nach Intervall [t, t+1) [kWh]': kumulierter_energieverbrauch_kWh}
-    umlauf_array = umlauf_array.append(neue_zeile, ignore_index=True)
+    liste.append(neue_zeile)
 
     # Berechnung der zurückgelegten Strecke und der neuen Ist-Geschwindigkeit
     zurueckgelegte_distanz += 0.5 * beschleunigung * (zeit_intervall ** 2) + v_ist * zeit_intervall
-    print("Zurückgelegte Strecke: ", zurueckgelegte_distanz, "m")
     v_ist += beschleunigung * zeit_intervall
-    print("Ist-Geschwindigkeit: ", v_ist, " m/s / ", v_ist * 3.6, " km/h")
     t += zeit_intervall
 
-    print("_________________________________________________________________________________________")
-
-print(umlauf_array)
-umlauf_array.to_excel('Output.xlsx')
+# Tabelle mit allen Daten des Umlaufs wird erstellt
+umlauf_tabelle = pd.DataFrame(liste)
+umlauf_tabelle.to_excel('Output.xlsx', sheet_name='Umlauf Nr.' + nummer)
+print('Laufzeit:', datetime.now() - start)

2e1fa4f36bf5335e4fdd9072433a78b50711d172
Fabio Frank <fabio.frank@hotmail.de>
2020-08-06 16:26:20 +0200

    energieverbrauch() aus umlauf() ausgelagert

--- Betriebstag.py
+++ Betriebstag.py
@@ -18,6 +18,7 @@ beschleunigung: float
 ladeleistung: float
 leistung_batterie: float
 liste: list
+energieverbrauch_im_intervall: float
 
 
 def pause(laenge):
@@ -38,7 +39,8 @@ def pause(laenge):
                       'Zeit [s]': t,
                       'SoC [%]': soc,
                       'Abgerufene Batterieleistung im Intervall [t, t+1) [kW]': ladeleistung_batterie / 1000,
-                      'Kumulierter Energieverbrauch nach Intervall [t, t+1) [KWh]': kumulierter_energieverbrauch / 3600000}
+                      'Kumulierter Energieverbrauch nach Intervall [t, t+1) [KWh]': kumulierter_energieverbrauch
+                                                                                    / 3600000}
         liste.append(neue_zeile)
         soc = Batterie.state_of_charge(energieaufnahme)
         uhrzeit += datetime.timedelta(seconds=zeit_intervall)
@@ -47,16 +49,16 @@ def pause(laenge):
     return pause_tabelle
 
 
-def bremsung(geschwindigkeit, verzoegerung):
+def anhalten(geschwindigkeit, verzoegerung):
     global t
     # Errechnen wie lange Bremsung mit gegebener Verzögerung von v_ist auf 0 dauert
     bremszeit = round(geschwindigkeit / verzoegerung)  # in ganzen Sekunden
-    for i in range(0, bremszeit):
-        t += zeit_intervall
+    return bremszeit
 
 
 def umlauf():
-    global soc, kumulierter_energieverbrauch, uhrzeit, t, zurueckgelegte_distanz, v_ist, v_soll, steigung, beschleunigung, leistung_batterie, liste, ladeleistung
+    global soc, kumulierter_energieverbrauch, uhrzeit, t, zurueckgelegte_distanz, v_ist, v_soll, steigung,\
+        beschleunigung, leistung_batterie, liste, ladeleistung
     streckenlaenge = Route.strecke['zurückgelegte Distanz [km]'].iloc[-1] * 1000
 
     # Initialisierung der Schleife
@@ -67,47 +69,69 @@ def umlauf():
     liste = []
 
     # Schleife, die läuft bis Umlauf beendet
-    # TODO: Halt an Bushaltestelle
     while zurueckgelegte_distanz < streckenlaenge:
-        # in Abhängigkeit der bereits zurückgelegten Distanz werden aktuelle Steigung sowie Soll-Geschwindigkeit aus der
-        # Routendatei ermittelt
-        v_soll = Route.v_soll(zurueckgelegte_distanz)
+        # TODO: Halt an Bushaltestelle (if-else: bremsen() vs. fahren())
+
+        # in Abhängigkeit der bereits zurückgelegten Distanz werden aktuelle Steigung sowie DWPT-Ladeleistung ermittelt
         ladeleistung = Route.dwpt_ladeleistung(zurueckgelegte_distanz)
         steigung = Route.steigung(zurueckgelegte_distanz)
-        # if Route.haltestelle(zurueckgelegte_distanz):
-        #     bremsung(v_ist, 2.0)
-        #     benoetigte_leistung = 20000  # Platzhalter
-        # else:
-            # Der Fahrer wählt in Abhängigkeit von Soll- und Ist-Geschwindigkeit eine Beschleunigung oder Verzögerung aus
-        beschleunigung = Fahrer.beschleunigung(v_ist, v_soll)
 
-        # Ermittlung des Gesamtleistungsbedarfs
-        fahrwiderstaende = Fahrzeug.fahrwiderstaende(v_ist, beschleunigung, steigung)
-        benoetigte_leistung = Elektromotor.leistung(fahrwiderstaende,
-                                                    v_ist) + Nebenverbraucher.leistung - ladeleistung
+        if Route.haltestelle(zurueckgelegte_distanz):
+            # Es wird ermittelt, wie lange gebremst werden musste, um das Fahrzeug zu Stillstand zu bringen
+            verzoegerung = 2.0
+            bremszeit = anhalten(v_ist, verzoegerung)
 
-        leistung_batterie = Batterie.leistung(benoetigte_leistung)
-        # Berechnung des Energieverbrauchs während des gewählten Zeitintervalls
-        energieverbrauch_im_intervall = leistung_batterie * zeit_intervall  # in Joule
+            # Nachträglich wird ermittelt, welche Energiemenge rekuperiert wurde
+            # Diese wird im Modell nun im Stillstand aufgenommen
+            # TODO: Work In Progress
 
-        # Aktualisieren des Gesamtenergieverbrauchs im Umlauf
-        kumulierter_energieverbrauch += energieverbrauch_im_intervall
+            # Der Bus muss stehen, sobald die Bushaltestelle erreicht ist.
+            beschleunigung = 0.0
+            v_ist = 0.0
+            v_soll = 0.0
 
-        # Gesammelte Daten abspeichern
-        daten_sichern()
+        else:
+            v_soll = Route.v_soll(zurueckgelegte_distanz)
 
-        # Laden bzw. Entladen der Batterie, Berechnung des neuen SoC
-        soc = Batterie.state_of_charge(energieverbrauch_im_intervall)
+            # Berechnung des Energieverbrauchs
+            energieverbrauch_im_intervall = energieverbrauch()
 
-        # Berechnung der zurückgelegten Strecke und der neuen Ist-Geschwindigkeit
-        zurueckgelegte_distanz += 0.5 * beschleunigung * (zeit_intervall ** 2) + v_ist * zeit_intervall
-        v_ist += beschleunigung * zeit_intervall
-        t += zeit_intervall
-        uhrzeit += datetime.timedelta(seconds=zeit_intervall)
+            # Aktualisieren des Gesamtenergieverbrauchs im Umlauf
+            kumulierter_energieverbrauch += energieverbrauch_im_intervall
+
+            # Gesammelte Daten abspeichern (wichtig: vor dem Aktualisieren des SoC)
+            daten_sichern()
+
+            # Laden bzw. Entladen der Batterie, Berechnung des neuen SoC
+            soc = Batterie.state_of_charge(energieverbrauch_im_intervall)
+
+            # Berechnung der zurückgelegten Strecke und der neuen Ist-Geschwindigkeit
+            zurueckgelegte_distanz += 0.5 * beschleunigung * (zeit_intervall ** 2) + v_ist * zeit_intervall
+            v_ist += beschleunigung * zeit_intervall
+            t += zeit_intervall
+            uhrzeit += datetime.timedelta(seconds=zeit_intervall)
 
     # Tabelle mit allen relevanten Daten des Umlaufs wird erstellt und zurückgegeben
     umlauf_tabelle = pd.DataFrame(liste)
     return umlauf_tabelle
+
+def energieverbrauch():
+    global soc, kumulierter_energieverbrauch, uhrzeit, t, zurueckgelegte_distanz, v_ist, v_soll, steigung, \
+        beschleunigung, leistung_batterie, liste, ladeleistung
+
+    # Der Fahrer wählt in Abhängigkeit von Soll- und Ist-Geschwindigkeit eine Beschleunigung oder Verzögerung aus
+    beschleunigung = Fahrer.beschleunigung(v_ist, v_soll)
+
+    # Ermittlung des Gesamtleistungsbedarfs
+    fahrwiderstaende = Fahrzeug.fahrwiderstaende(v_ist, beschleunigung, steigung)
+    benoetigte_leistung = Elektromotor.leistung(fahrwiderstaende,
+                                                v_ist) + Nebenverbraucher.leistung - ladeleistung
+
+    leistung_batterie = Batterie.leistung(benoetigte_leistung)
+
+    # Berechnung des Energieverbrauchs während des gewählten Zeitintervalls
+    return leistung_batterie * zeit_intervall  # in Joule
+
 
 
 def daten_sichern():

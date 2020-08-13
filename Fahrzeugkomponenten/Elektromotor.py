3aa325eab2b68c27ee2c096bc28908f70b70632b
Fabio Frank <fabio.frank@hotmail.de>
2020-08-13 17:36:52 +0200

    Anpassung Wirkungsgrad EM

--- Fahrzeugkomponenten/Elektromotor.py
+++ Fahrzeugkomponenten/Elektromotor.py
@@ -8,7 +8,7 @@ from Fahrzeugkomponenten import Getriebe
 maximale_leistung = 160000.0 # TODO: Quelle
 
 # Wirkungsgrad des Elektromotors, konstant angenommen
-effizienz = 0.95 # TODO: Wirkungsgrad aus Primovedaten
+effizienz = 0.82 # Mittelwert aus Primove-Datensatz
 
 # Wirkungsgrad des Getriebes
 Getriebe.effizienz = 0.95 # TODO: Quelle Getriebeverluste
@@ -18,12 +18,15 @@ Getriebe.effizienz = 0.95 # TODO: Quelle Getriebeverluste
 
 # Berechnung der Leistung des Elektromotors unter Berücksichtigung von Verlusten
 def leistung(fahrwiderstaende, v_ist):
+    # Betrieb als Generator
     if fahrwiderstaende < 0:
         benoetigte_leistung_em = fahrwiderstaende * v_ist * effizienz * Getriebe.effizienz
         if abs(benoetigte_leistung_em) <= maximale_leistung:
             reale_leistung_em = benoetigte_leistung_em
         else:
             reale_leistung_em = - maximale_leistung
+
+    # Betrieb als Motor
     else:
         benoetigte_leistung_em = (fahrwiderstaende * v_ist) / (effizienz * Getriebe.effizienz)
         if benoetigte_leistung_em <= maximale_leistung:
